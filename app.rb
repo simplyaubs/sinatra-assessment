4a32c7d409c1be69d62aa938ed5aeebdca2fc386
Aubrey Howell <simplyaubs@gmail.com>
2014-03-31 09:58:26 -0600

    User can add products and see them on the homepage

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require 'sinatra/base'
 
-class App < Sinatra::Base
+class App < Sinatra::Application
+  PRODUCTS = []
   get '/' do
-    "Welcome"
+    erb :index, :locals => {:products => PRODUCTS}
+  end
+  get '/new' do
+    erb :add_product
+  end
+  post '/' do
+    PRODUCTS << params[:product]
+    redirect '/'
   end
 end
--- spec/products_spec.rb
+++ spec/products_spec.rb
@@ -6,6 +6,9 @@ Capybara.app = App
 feature "User can manage products" do
   scenario "User sees a page that says 'Welcome'" do
     visit '/'
+    click_on "Add a Product"
+    fill_in "product", with: "MacBook"
+    click_on "Create Product"
     expect(page).to have_content("Welcome")
   end
 
